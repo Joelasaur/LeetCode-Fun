ff6c5a9716bfaf11fd27d32ce1abd7b37eb0f830
Joel Whalen <jowhalen13@gmail.com>
2019-02-26 20:28:09 -0600

    uniqueEmailAddresses

---
+++ UniqueEmailAddresses/test.js
@@ -0,0 +1,19 @@
+
+const assert = require('assert');
+const uniqueEmails = require('./uniqueEmails');
+
+describe('Unique Email Addresses', function() {
+  describe('#uniqueEmails()', function() {
+    it ('should validate . in the local name', function() {
+        assert.equal(uniqueEmails(['test.email@gmail.com', 'test.oiewfjoiejf@leet.net']), 2)
+    });
+    it('should validate ., +, and @ correctly', function() {
+      assert.equal(uniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]), 2);
+      assert.equal(uniqueEmails(["testemail@leetcode.com","testemail1@leetcode.com","testemail+david@lee.tcode.com"]), 3);
+    });
+    it('should allow unique domain names but duplicate local names', function() {
+        assert.equal(uniqueEmails(["testemail@leetcode.com","testemail1@leetcode.com","testemail+david@lee.tcode.com"]), 3);
+      });
+  });
+});
+
---
+++ UniqueEmailAddresses/uniqueEmails.js
@@ -0,0 +1,44 @@
+// Every email consists of a local name and a domain name, separated by the @ sign.
+
+// For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+
+// Besides lowercase letters, these emails may contain '.'s or '+'s.
+
+// If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be 
+// forwarded to the same address without dots in the local name.  
+// For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+
+// If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered,
+// for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+// It is possible to use both of these rules at the same time.
+// Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails? 
+// Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+// Output: 2
+// Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+
+// Runtime: 104 ms, faster than 37.44% of JavaScript online submissions for Unique Email Addresses.
+// Memory Usage: 43.8 MB, less than 17.30% of JavaScript online submissions for Unique Email Addresses.
+/**
+ * Every email address is valid. How many unique mails are actually sent?
+ * @param {string[]} emails
+ * @return {number}
+ */
+const numUniqueEmails = function(emails) {
+    let uniqueEmails = new Set();
+    for (email of emails) {
+        // Split up local name and domain name using the @ symbol
+        let names = email.split('@');
+        let localName = names[0];
+        let domainName = names[1];
+        // Remove all instances of . from local name
+        localName = localName.split('.').join('');
+        // Remove everything after the +
+        localName = localName.split('+')[0];
+        // Add the revised local name to a set
+        uniqueEmails.add(localName + '@' + domainName);
+    }
+    // Return the Set's size
+    return uniqueEmails.size;
+};
+
+module.exports = numUniqueEmails;
